ca0ec7534a422c9a7c2ed480c357166a813d5aca
Denis Aguilar <denis.ragui@gmail.com>
2020-12-19 21:58:17 +0100

    fixed transaction account reference

--- src/Domain/Entities/Transaction.cs
+++ src/Domain/Entities/Transaction.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace GreenCrop.Domain.Entities {
     public class Transaction {
         public Guid Id { get; set; }
-        public string Account { get; set; }
+        public Account Account { get; set; }
         public string AccountId { get; set; }
         public string Message { get; set; }
     }
